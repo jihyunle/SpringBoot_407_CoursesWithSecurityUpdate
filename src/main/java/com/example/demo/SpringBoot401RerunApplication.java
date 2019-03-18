b5937be736580c86a6bedc5caf6f83c03f507c9c
jihyunle <jennifer.jlee@outlook.com>
2019-03-18 09:59:34 -0400

    Initial commit; Finished 4.01

---
+++ src/main/java/com/example/demo/SpringBoot401RerunApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot401RerunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot401RerunApplication.class, args);
+    }
+
+}
