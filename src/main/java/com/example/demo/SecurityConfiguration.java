080736cf244ed17409c00a6f228a435fe636d7f6
jihyunle <jennifer.jlee@outlook.com>
2019-03-18 10:13:54 -0400

    added comment line explanations

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,3 +1,5 @@
+// this java class sets up the app to restrict access. by default, if access is not specified, it is denied.
+
 package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
@@ -10,20 +12,29 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+// above annots indicate to compiler that file is configuration file and that SpringSecurity is enabled for this app
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    // extended class has all the methods needed to include security in our app
 
+    // the @Bean annot and passwordEncoder() method creates an obj that can be re-used to encode passwords in your app
+    // provides an instance of BCryptPasswordEncoder, which is a pw encoder that uses the BCrypt hashing function
     @Bean
     public static BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    // The configure() method configures users who can access the app.
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin();
+                .authorizeRequests() // tells your app which requests should be authorized
+                .anyRequest().authenticated() // in this case, any request that is authenticated should be permitted
+                .and() // adds addt'l rules
+                .formLogin(); // tells spring app should show log in form
     }
+
+    // multiple users can be configured here but atm it is for single in-memory user
+    // you can further specify how users are granted access to app if their details are stored in db
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication().withUser("user")
