5b5373d06dbf293a028f5f10b84bc8be060fed8e
jihyunle <jennifer.jlee@outlook.com>
2019-03-18 11:06:51 -0400

    Added admin and user2

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -13,4 +13,8 @@ public class HomeController {
     public String login(){
         return "login";
     }
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
 }
--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -26,27 +27,51 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // The configure() method configures users who can access the app.
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http
-                .authorizeRequests() // tells your app which requests should be authorized
+        // Restricts access to routes
+        http.authorizeRequests()
+                .antMatchers("/")
+                .access("hasAnyAuthority('USER','ADMIN')")
+                // .access() breaks if I also add 'USER2'
+                // 'USER' is a type of authority and includes "USER2"
+                .antMatchers("/admin").access("hasAuthority('ADMIN')")
+
                 .anyRequest().authenticated() // in this case, any request that is authenticated should be permitted
                 .and() // adds addt'l rules
-                .formLogin().loginPage("/login").permitAll(); // means you are expecting a login form,
-                                                            // which will display when you visit the route /login and
-                                                            // everyone can see it even if they're not authenticated
-                                                            // this is the page ppl see if they've not logged in yet
+                .formLogin().loginPage("/login").permitAll()
+                // this is the page ppl see if they've not logged in yet
+                // means you are expecting a login form, which will display when you visit the route /login and
+                // everyone can see it even if they're not authenticated
+
+                .and()
+                .logout()
+                .logoutRequestMatcher(
+                        new AntPathRequestMatcher("/logout")
+
+                )
+                .logoutSuccessUrl("/login").permitAll();
     }
 
     // multiple users can be configured here but atm it is for single in-memory user
     // you can further specify how users are granted access to app if their details are stored in db
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication().withUser("user")
-                .password(passwordEncoder().encode("password"))
-                .authorities("USER");
 
-        // add another user
-        auth.inMemoryAuthentication().withUser("admin")
-                .password(passwordEncoder().encode("masterkey"))
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .authorities("USER")
+
+                .and()
+                // add another user
+                .withUser("user2")
+                .password(passwordEncoder().encode("password2"))
+                .authorities("USER")
+                // .authorities() defines the access type
+
+                .and()
+                // add admin
+                .withUser("admin")
+                .password(passwordEncoder().encode("masterpassword"))
                 .authorities("ADMIN");
     }
 }
